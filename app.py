f424d8158dc642d71a63a182db523bc9e7b8b51e
passonigioele <65167694+passonigioele@users.noreply.github.com>
2025-11-18 09:31:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ if uploaded_file:
 st.subheader("Veterans")
 top_players_df = filtered_players_df.sort_values(by="Match Played", ascending=False).head(5)
 columns_to_display2 = ["Player Name", "Match Played"]
-filtered_players_df = filtered_players_df[columns_to_display2]
+top_players_df = top_players_df[columns_to_display2]
 
 # Render using AgGrid for consistency
 gb_top = GridOptionsBuilder.from_dataframe(top_players_df)
