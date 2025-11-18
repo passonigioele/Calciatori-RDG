b3979915575c42c38e2e50531ecd753762501fb2
passonigioele <65167694+passonigioele@users.noreply.github.com>
2025-11-18 09:34:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -113,3 +113,39 @@ AgGrid(
     fit_columns_on_grid_load=False
 )
 
+
+# Top 5 players by Goal Scored
+st.subheader("Capocannonieri")
+top_goals_df = filtered_players_df.sort_values(by="Goal Scored", ascending=False).head(5)
+columns_to_display3 = ["Player Name", "Goal Scored"]
+top_goals_df = top_goals_df[columns_to_display3]
+
+# Render using AgGrid for consistency
+gb_top = GridOptionsBuilder.from_dataframe(top_goals_df)
+gb_top.configure_default_column(editable=False, sortable=True, filter=False)
+gb_top.configure_grid_options(domLayout='normal')
+gb_top.configure_grid_options(suppressHorizontalScroll=False)
+
+# Column alignment
+for i, col in enumerate(top_players_df.columns):
+    if i == 0:
+        gb_top.configure_column(col, minWidth=150, cellStyle={'textAlign': 'left'})
+    else:
+        gb_top.configure_column(col, minWidth=120, cellStyle={'textAlign': 'center'})
+
+gridOptions_top = gb_top.build()
+gridOptions_top['suppressAutoSize'] = True
+gridOptions_top['suppressSizeToFit'] = True
+
+AgGrid(
+    top_goals_df,
+    gridOptions=gridOptions_top,
+    enable_enterprise_modules=False,
+    height=180,  # Smaller height for top 5
+    fit_columns_on_grid_load=False
+)
+
+
+
+
+
